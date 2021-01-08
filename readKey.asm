6ee8a70d3607e36c9999aef51f97e337b289b611
Lunarspear <armand.camner@gmail.com>
2021-01-08 10:58:33 +0200

    Some small changes

--- readKey.asm
+++ readKey.asm
@@ -3,72 +3,74 @@ DATAM SEGMENT
 	hello 				DB 'This program was done by $'
 	author1 			DB 'Camner Armand Gabriel$' 
 	author2 			DB 'Monica Ioana Vulpe$' 
-	program 			DB 'Please Read the following table content in order to run the program:'
+	program 			DB 'Programs:$'
 	program1 			DB 'Base64$'
 	program2 			DB 'Vigenere$' 
 	program3 			DB 'Playfair$'
 	wtd					DB 'What do you want to do?$'
-	input1 	 			DB 30 dup(1) 
-	input2				DB 30 dup(2)
-	input3				DB 30 dup(3)
+	input1 	 			DB 16 dup(1) 
+	input2				DB 16 dup(2)
+	input3				DB 16 dup(3)
 DATAM ENDS
 
 STACKSEGMENTM SEGMENT
+	DW 100 dup (?)
 	startStack label word ; eticheta de 2 bytes ( 16 ) biti care indica adreasa 0000
-	dw 124 dup (1)
 STACKSEGMENTM ENDS
 
 
 CODM SEGMENT
-ASSUME DS:DATAM,CS:CODM,SS:STACKSEGMENTM
+ASSUME CS:CODM,SS:STACKSEGMENTM,DS:DATAM
 START:
-	MOV AX, SEG DATAM
+	MOV AX, DATAM
 	MOV DS, AX
-	MOV AX, seg STACKSEGMENTM
+	
+	MOV AX, STACKSEGMENTM
 	MOV SS, AX
-	MOV SP, offset startStack 
+	lea AX, startStack 
+	MOV SP, AX
 	
-	MOV AX, SEG DATAM
+	MOV AX,  DATAM           	;0
 	PUSH AX
-	LEA AX, input1
-	PUSH AX
-	
-	MOV AX, SEG DATAM
-	PUSH AX
-	LEA AX, input2
+	LEA AX, input1				;1
 	PUSH AX
 	
-	MOV AX, SEG DATAM
-	PUSH AX
-	LEA AX, input3
-	PUSH AX
+	;MOV AX, SEG DATAM			;2
+	;PUSH AX
+	;LEA AX, input2				;3
+	;PUSH AX
 	
-	MOV AX, SEG DATAM					; 
-	PUSH AX
+	;MOV AX, SEG DATAM			;4
+	;PUSH AX
+	;LEA AX, input3				;5
+	;PUSH AX
 	
-	LEA AX, hello						; 
-	PUSH AX
+	;MOV AX, SEG DATAM			;6	
+	;PUSH AX
 	
-	LEA AX, author1
-	PUSH AX
+	;LEA AX, hello				;7	
+	;PUSH AX
 	
-	LEA AX, author2
-	PUSH AX
+	;LEA AX, author1				;8
+	;PUSH AX
 	
-	LEA AX, program
-	PUSH AX
+	;LEA AX, author2				;9
+	;PUSH AX
 	
-	LEA AX, program1
-	PUSH AX
+	;LEA AX, program				;10
+	;PUSH AX
 	
-	LEA AX, program2
-	PUSH AX
+	;LEA AX, program1			;11
+	;PUSH AX
 	
-	LEA AX, program3
-	PUSH AX
+	;LEA AX, program2			;12
+	;PUSH AX
 	
-	LEA AX, wtd
-	PUSH AX
+	;LEA AX, program3			;13
+	;PUSH AX
+	
+	;LEA AX, wtd					;14
+	;PUSH AX
 		
 	CALL FAR PTR ABOUT
 	CALL PRINTNEWLINE
@@ -78,11 +80,13 @@ START:
 ABOUT PROC FAR
 	PUSH BP
 	MOV BP, SP
-	LEA DX, SS:[BP+2]
+	LEA DX, SS:[BP+1]
 	MOV ah, 09h
 	INT 21h	
+	POP BP
 	CALL PRINTNEWLINE
-	LEA DX, SS:[BP+4]
+	PUSH BP
+	LEA DX, SS:[BP+2]
 	MOV ah, 09h
 	INT 21h	
 	
